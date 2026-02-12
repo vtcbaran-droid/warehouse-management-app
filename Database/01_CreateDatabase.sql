d6ec17589ec0367097f8b96c366d9ed05794e3b8
vtcbaran-droid <vtc.baran@gmail.com>
2026-02-12 17:59:50 +0530

    Initial SQL script to create the WMA database and initial tables

---
+++ Database/01_CreateDatabase.sql
@@ -0,0 +1,6 @@
+-- SQL script to create the Warehouse Management App database and initial tables
+CREATE DATABASE WarehouseManagementApp;
+
+USE WarehouseManagementApp;
+
+-- Here you can add more initial configurations if needed.
